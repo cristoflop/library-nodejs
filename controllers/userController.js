f11e5007790c9bdd4d91ab4abf28fb3d4ba30b2c
cristoflop <cristofer.lc.99@gmail.com>
2020-12-15 20:37:32 +0100

    Added test method async await

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,9 +2,29 @@
 
 const validator = require("express-validator");
 
-function getUsers(request, response) {
-    response.sendStatus(200);
-    response.send("usuarios", ["cristofer", "juan", "luis", "mica"]);
+async function getUsers(request, response) {
+    response.status(200);
+    let usuarios = await test();
+    response.json(usuarios);
+}
+
+function test() {
+    return {
+        users: [
+            {
+                name: "cristofer"
+            },
+            {
+                name: "juan"
+            },
+            {
+                name: "luis"
+            },
+            {
+                name: "mica"
+            }
+        ]
+    };
 }
 
 module.exports = {
