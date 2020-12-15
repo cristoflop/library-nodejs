b06ef49dbc20a53a0a257712d3f2bce52848fa62
cristoflop <cristofer.lc.99@gmail.com>
2020-12-15 19:36:27 +0100

    Added app configuration to start the app

--- config.js
+++ config.js
@@ -4,8 +4,6 @@ module.exports = {
     dbConfig: {
         host: "localhost",
         databaseServerPort: 27017,
-        user: "root",
-        password: "root",
         database: "library"
     },
     port: 3000
