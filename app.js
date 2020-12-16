f6c34b70558dc9a863a6057af400c5d0b86c94e5
cristoflop <cristofer.lc.99@gmail.com>
2020-12-16 18:21:53 +0100

    Added connection in app.js and deleted unused libraries

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 "use strict"
 
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
 const app = express();
-const config = require("./config");
 
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const config = require("./config");
+const url = `mongodb://${config.dbConfig.host}:${config.dbConfig.databaseServerPort}/${config.dbConfig.database}aaa`;
+const mongoose = require("mongoose");
+mongoose.connect( // no hace falta el then
+    url,
+    {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
+const database = mongoose.connection;
 
 const session = require("express-session");
 const mongoDBSession = require("connect-mongodb-session");
 const MongoDBStore = mongoDBSession(session);
 const sessionStore = new MongoDBStore({
-    uri: `mongodb://${config.dbConfig.host}:${config.dbConfig.databaseServerPort}/${config.dbConfig.database}`,
+    uri: url,
     collection: "sessions"
 })
 
@@ -30,15 +38,34 @@ const userRouter = require("./routes/userRouter");
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
 
 app.use('/api', userRouter);
 
-app.listen(config.port, function (err) {
-    if (err)
-        console.error(`No se ha podido iniciar el servidor: ${err.message}`)
-    else
-        console.log(`Servidor arrancando en el puerto ${config.port}`)
+app.use(middlewareNotFound);
+app.use(middlewareServerError);
+
+function middlewareNotFound(request, response) {
+    let error = new Error("Not found")
+    response.status(404);
+    response.json(error);
+}
+
+function middlewareServerError(request, response) {
+    let error = new Error("Server error")
+    response.status(500);
+    response.json(error);
+    response.end();
+}
+
+database.on("error", function () {
+    console.error("Error al conectar con la bd");
+})
+
+database.once("open", function () {
+    app.listen(config.port, function (err) {
+        if (err)
+            console.error(`No se ha podido iniciar el servidor: ${err.message}`)
+        else
+            console.log(`Servidor arrancando en el puerto ${config.port}`)
+    });
 });
