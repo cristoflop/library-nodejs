06248a7b0f7e5fce5997d065cb68ad056dd0ec56
cristoflop <cristofer.lc.99@gmail.com>
2021-01-17 16:56:02 +0100

    Added login and register imp

--- controllers/loginRegisterController.js
+++ controllers/loginRegisterController.js
@@ -1,11 +1,43 @@
 "use strict"
 
-function login(request, response) {
+const User = require("../models/user");
+
+async function login(request, response) {
     // login
+    const nick = request.body.nick;
+    const password = request.body.password;
+    let user = await User.findOne({nick, password});
+    if (user == null) {
+        response.status(400);
+        response.json({message: `Authentication error, check nick or password`});
+        return;
+    }
+    request.user = user._id; // loged
+    response.status(200);
+    response.location(`${request.baseUrl + request.path}/${user._id}`);
+    response.json();
 }
 
-function register(request, response) {
+async function register(request, response) {
     // register
+    const nick = request.body.nick;
+    const password = request.body.password;
+    let user = await User.findOne({nick});
+    if (user != null) {
+        response.status(409);
+        response.json({message: `User with nick '${nick}' already exists`});
+        return;
+    }
+
+    user = await new User({
+        nick: request.body.nick,
+        email: request.body.email,
+        password: password
+    }).save();
+
+    response.status(201);
+    response.location(`${request.baseUrl + request.path}/${user._id}`);
+    response.json();
 }
 
 module.exports = {
