380af71b35412511507ec5098c13fd964afccb6d
cristoflop <cristofer.lc.99@gmail.com>
2021-01-18 15:02:32 +0100

    Removed accesscontrol library and added authentication function

---
+++ auth.js
@@ -0,0 +1,14 @@
+"use strict"
+
+function authUser(request, response, next) {
+    if (request.user == null) {
+        response.status(403); // forbidden
+        response.json({message: "You need to sign in"});
+        return;
+    }
+    next();
+}
+
+module.exports = {
+    authUser
+}
